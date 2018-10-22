d8b8e19f131ff2aca0a6a61748b96b50f4f761f1
sdj01044 <sdj01044@naver.com>
2018-10-23 02:16:26 +0900

    modify mychart.js
    add tooltip 2월~10월's keywords

--- jeju/web/mychart.js
+++ jeju/web/mychart.js
@@ -11,6 +11,53 @@ var myChart = {
     "value-box":{
       "text":"%v"
     },
+    "tooltip":{
+      "rules": [{ //Rule 1
+        "rule": "%v == 6457",
+        "text": "2월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 2
+        "rule": "%v == 9076",
+        "text": "3월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 3
+        "rule": "%v == 5218",
+        "text": "4월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 4
+        "rule": "%v == 13823",
+        "text": "5월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 5
+        "rule": "%v == 38288",
+        "text": "6월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 6
+        "rule": "%v == 14798",
+        "text": "7월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 7
+        "rule": "%v == 13366",
+        "text": "8월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 8
+        "rule": "%v == 21018",
+        "text": "9월의 키워드",
+        "background-color": "#000000"
+      }, { //Rule 9
+        "rule": "%v == 26697",
+        "text": "10월의 키워드",
+        "background-color": "#000000"
+      }],
+      "font-color": "white",
+      "font-size" : "13",
+      "font-weight": "bold",
+      "alpha": 0.5,
+      "border-color": "#000001",
+      "border-width": 1,
+      "padding": "20%",
+      "border-radius": "9px"
+    },
     "animation": {
       "delay": "100",
       "effect": "4",
